15e7bee537a9bd83320f73e9a468b720a2843786
Jeremy Her <91761028+herj043@users.noreply.github.com>
2022-02-07 23:25:35 +0000

    Inserted input text. Remember to learn and insert event loops

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 import PySimpleGUI as sg
 
+# layout the insides of the window
 layout = [[sg.Text('Input text test.')],
-                  [sg.InputText()],
-                  [sg.Submit(), sg.Cancel()]]      
+  [sg.Text("BMI Calculator")],
+  [
+    sg.Text(
+      'Enter your age, height, and weight to get your Body Mass Index value and description',
+      size=(40, 3)
+    )
 
+  ],
+  [
+    sg.Text("Age"),
+    sg.Input(size=(3, 1), key='-AGE-')
+  ],
+  [sg.InputText()],
+  [sg.Submit(), sg.Cancel()]]      
+
+# Inputs everything noted from "layout"
 window = sg.Window('PySimpleGUI Window', layout)    
 
+# event command inserts whatever the user inputs
 event, values = window.read()    
 window.close()
 
