58b26a7490e46fedf04b9aa9b8d4db4be5d14944
Jeremy Her <91761028+herj043@users.noreply.github.com>
2022-04-11 18:33:21 +0000

    Leaning tkinter

--- main.py
+++ main.py
@@ -1,28 +1,42 @@
-import PySimpleGUI as sg
+from tkinter import *
+from functions import *
 
-# layout the insides of the window
-layout = [[sg.Text('Input text test.')],
-  [sg.Text("BMI Calculator")],
-  [
-    sg.Text(
-      'Enter your age, height, and weight to get your Body Mass Index value and description',
-      size=(40, 3)
-    )
+win = Tk()
 
-  ],
-  [
-    sg.Text("Age"),
-    sg.Input(size=(3, 1), key='-AGE-')
-  ],
-  [sg.InputText()],
-  [sg.Submit(), sg.Cancel()]]      
+# Counts the number of clicks
+pre = 0
 
-# Inputs everything noted from "layout"
-window = sg.Window('PySimpleGUI Window', layout)    
+def click():
+  global pre
+  pre += 1
 
-# event command inserts whatever the user inputs
-event, values = window.read()    
-window.close()
+  
+  my_label = Label(win, text=str(got) + str(pre), bg="#ffffff")
+  my_label.pack()
 
-text_input = values[0]    
-sg.popup('You entered', text_input)
+  if pre % 7 == 1:
+    win.config(bg="#0000ff")
+  elif pre % 7 == 2:
+    win.config(bg="#00ff00")
+  elif pre % 7 == 3:
+    win.config(bg="#ff0000")
+  elif pre % 7 == 4:
+    win.config(bg="#00ffff")
+  elif pre % 7 == 5:
+    win.config(bg="#ffff00")
+  elif pre % 7 == 6:
+    win.config(bg="#ffff00")
+  else:
+    win.config(bg="#ffffff")
+
+
+txt_box = Entry(win, width=30, bg="white")
+txt_box.pack()
+txt_box.insert(0, text="Enter Text")
+got = txt_box.get() + " "
+
+# Creates button
+my_button = Button(win, text="Print!",padx=20, pady= 20, command=click)
+my_button.pack()
+
+win.mainloop()
